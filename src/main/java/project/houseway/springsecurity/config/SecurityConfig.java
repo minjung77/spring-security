30cdc8676b75fe69851d85463fac1b603fa918e1
min <fheka77@gmail.com>
2025-03-21 12:10:39 +0900

    커스텀 폼 로그인 인증 with JPA, Role

--- src/main/java/project/houseway/springsecurity/config/SecurityConfig.java
+++ src/main/java/project/houseway/springsecurity/config/SecurityConfig.java
@@ -34,10 +34,10 @@ public class SecurityConfig {
                     .antMatchers("/user/**").hasRole("USER")// user 권한 사용자만 접근
                     .antMatchers("/admin/**").hasRole("ADMIN")
                     .antMatchers("/logout").authenticated()// 인증 받은 사용자감 접근 가능
-                    .antMatchers("/**").permitAll()// 인증/인가 여부와 상관없이 접근 가능
+                    .antMatchers("/**", "/member/login").permitAll()// 인증/인가 여부와 상관없이 접근 가능
                     .and()
                 .formLogin()
-                    .loginProcessingUrl("/api/login")//로그인 처리 url
+                    .loginPage("/member/login") //커스텀 로그인 페이지 설정
                     .usernameParameter("userid") //아이디 매개변수 지정
                     .passwordParameter("passwd") //비밀번호 매개변수 지정
                     .defaultSuccessUrl("/")//로그인 성공 시 리다이텍트 url
--- src/main/java/project/houseway/springsecurity/controller/IndexController.java
+++ src/main/java/project/houseway/springsecurity/controller/IndexController.java
@@ -1,4 +1,4 @@
-package project.houseway.springsecurity;
+package project.houseway.springsecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
